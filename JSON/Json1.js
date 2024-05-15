2ac43945333fb83d19741230de1da7c748b26004
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-15 16:27:52 +0530

    JSON

---
+++ JSON/Json1.js
@@ -0,0 +1,16 @@
+var student = {
+    name:"Rahul",
+    age:20,
+    address:"Delhi",
+    phone:76984798347,
+}
+
+console.log(student);
+
+console.log(student.name);
+
+console.log(student.age);
+
+console.log(student.address);
+
+console.log(student.phone);
---
+++ JSON/Json2.js
@@ -0,0 +1,28 @@
+const product = {
+
+    name:"I Phone",
+    price:10000,
+    size:"10 inch",
+    brand:"Apple",
+    address:{
+        city:"Ahmedabad",
+        state:"Gujrat",
+        country:"India",
+        pincode:380001
+    },
+    color:["red","white","black","purple"]
+
+}
+
+console.log(product);
+
+console.log(product.name);
+
+console.log(product.price);
+
+console.log(product.address);
+
+console.log(product.address.city);
+console.log(product.color);
+
+console.log(product.color[2]);
