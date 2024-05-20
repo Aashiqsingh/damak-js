7767ac8d03ae2f2d191cb8af01ed418191e509d9
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-20 16:36:32 +0530

    array

---
+++ Array/array1.js
@@ -0,0 +1,29 @@
+var arr = ["damak","yukti","tirth","zankhan","kamal","priya","rahul" , 23 , true ]
+
+// console.log(arr);
+
+// console.log(arr[0]);
+
+// console.log(arr[1]);
+
+// console.log(arr[2]);
+
+// console.log(arr[3]);
+
+// console.log(arr[4]);
+
+// for(let i=0;i<arr.length;i++){
+
+//     console.log(arr[i]);
+// }
+console.log(arr);
+
+// arr.push("ram")
+// console.log(arr);
+
+
+// var x = arr.push("krishna")
+// console.log(x);
+
+arr.pop()
+console.log(arr);
---
+++ Array/array2.js
@@ -0,0 +1,26 @@
+// var arr = ["damak","yukti","tirth","zankhan","kamal","priya","rahul" , 23 , true ]
+
+// console.log(arr);
+// console.log(arr.length);
+
+// // arr.unshift("Ram")
+// // console.log(arr);
+
+// // var x = arr.unshift("krishna")
+// // console.log(x);
+
+// arr.shift()
+// console.log(arr);
+// console.log(arr.length);
+
+var users = ["raj","krish","simran","payal","priya","akash","pooja","rahul"]
+
+console.log(users);
+
+// var x = users.join()
+// console.log(x);
+
+// console.log(x[3]);
+
+var x = users.join("")
+console.log(x);
