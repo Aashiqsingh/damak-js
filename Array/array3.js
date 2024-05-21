dfbe3897df7c578b0efda22b9ef0108f790698ef
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-21 16:38:48 +0530

    splice,slice

---
+++ Array/array3.js
@@ -0,0 +1,33 @@
+var lang =["hindi","english","bengali","tamil","urdu","gujarati","marathi"]
+
+console.log(lang);
+
+// var x = lang.splice(1,1)
+// console.log(x);
+
+// lang.splice(1,1,"korean","bengali")
+// console.log(lang);
+
+// var color = ["red","yellow","green","blue"]
+// var x = lang.concat(color)
+// console.log(x);
+
+// var x = lang.includes("marathi")
+// console.log(x);
+
+// var x = lang.indexOf("urdu")
+// console.log(x);
+
+// var x = lang.reverse()
+// console.log(x);
+
+// var x = lang.slice(1,5)
+// console.log(x);
+
+var x = lang.slice(1)
+console.log(x);
+
+var x = lang.toString()
+console.log(x);
+
+console.log(x[0]);
