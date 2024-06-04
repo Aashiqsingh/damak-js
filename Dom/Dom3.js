22bd3dd344c822101bfaf78b77f7f212e28d85d3
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-04 16:51:01 +0530

    move,mouseup,down

---
+++ Dom/Dom3.js
@@ -0,0 +1,14 @@
+const myDiv = document.getElementById("myDiv")
+
+
+function change(){
+
+    console.log("Mouse is moving...");
+}
+
+
+const myImg = document.getElementById("myImg")
+function changeSize(){
+    myImg.style.height = "200px"
+    myImg.style.width = "200px"
+}
