63ae15c40966794300d7c8616d6da626c646efeb
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-05 16:47:27 +0530

    task

---
+++ Task/task.js
@@ -0,0 +1,34 @@
+// var colors = ["red","pink","green","yellow","blue","cyan","lime"]
+
+// function change(){
+
+//     const mydiv = document.getElementById("mydiv")
+//     const random = Math.floor(Math.random() * colors.length)
+//     // console.log("random",random);
+
+//     mydiv.style.backgroundColor = colors[random]
+// }
+
+
+
+
+
+var images = ["img1.jpg","img2.jpg","img3.jpeg","img4.avif","img5.jpg"]
+
+function change(){
+
+    const myimg = document.getElementById("myimg")
+
+    const random = Math.floor(Math.random() * images.length)
+
+    var path = "../Images/"+ images[random]
+
+    myimg.src = path
+    
+
+}
+
+
+
+
+
