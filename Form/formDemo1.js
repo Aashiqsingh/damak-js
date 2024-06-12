8128329e4276c489830e18a31a95ab8fdf2678c1
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-12 16:42:32 +0530

    formValidation

--- Form/formDemo1.js
+++ Form/formDemo1.js
@@ -1,6 +1,42 @@
+const submitHandler = (e)=>{
+    e.preventDefault();  // prevent default
 
-function callFun(){
-    var input = document.getElementById("ans")
-    console.log(input.value);
+
+    const name = document.getElementById("name")
+    console.log("Name = ",name.value);
+    const nameErr = document.getElementById("nameErr")
+    const age = document.getElementById("age")
+    const ageErr = document.getElementById("ageErr")
+
+    if(name.value.length == ""){
+        nameErr.innerHTML = "<font color='red'>Please enter name <font>"
+        name.style.outline = "2px solid red"
+    }
+    else if(name.value.length  < 4 ){
+        nameErr.innerHTML = "<font color='red'>Name should be atleast 4 characters <font>"
+        name.style.outline = "2px solid red"
+    }
+    else{
+        nameErr.innerHTML = ""
+        name.style.outline = "2px solid green"
+    }
+
     
+    if(age.value == ""){
+        ageErr.innerHTML = "<font color='red'>Please enter age <font>"
+        age.style.outline = "2px solid red"
+    }
+    else if(age.value < 18){
+        ageErr.innerHTML = "<font color='red'>You are kid... <font>"
+        age.style.outline = "2px solid red"
+    }
+    else{
+        ageErr.innerHTML = ""
+        age.style.outline = "2px solid green"
+    }
+
+
+
+    // alert("form submitted....")
+    console.log("form submitted....");
 }
