1fb01cb22d9d8bd92c6c5c21481489a80b095bed
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-12 16:57:07 +0530

    form

--- Form/formDemo1.js
+++ Form/formDemo1.js
@@ -3,40 +3,58 @@ const submitHandler = (e)=>{
 
 
     const name = document.getElementById("name")
-    console.log("Name = ",name.value);
+    // console.log("Name = ",name.value);
     const nameErr = document.getElementById("nameErr")
     const age = document.getElementById("age")
     const ageErr = document.getElementById("ageErr")
 
-    if(name.value.length == ""){
-        nameErr.innerHTML = "<font color='red'>Please enter name <font>"
-        name.style.outline = "2px solid red"
-    }
-    else if(name.value.length  < 4 ){
-        nameErr.innerHTML = "<font color='red'>Name should be atleast 4 characters <font>"
-        name.style.outline = "2px solid red"
-    }
-    else{
-        nameErr.innerHTML = ""
-        name.style.outline = "2px solid green"
-    }
+    // if(name.value.length == ""){
+    //     nameErr.innerHTML = "<font color='red'>Please enter name <font>"
+    //     name.style.outline = "2px solid red"
+    // }
+    // else if(name.value.length  < 4 ){
+    //     nameErr.innerHTML = "<font color='red'>Name should be atleast 4 characters <font>"
+    //     name.style.outline = "2px solid red"
+    // }
+    // else{
+    //     nameErr.innerHTML = ""
+    //     name.style.outline = "2px solid green"
+    // }
 
     
-    if(age.value == ""){
-        ageErr.innerHTML = "<font color='red'>Please enter age <font>"
-        age.style.outline = "2px solid red"
-    }
-    else if(age.value < 18){
-        ageErr.innerHTML = "<font color='red'>You are kid... <font>"
-        age.style.outline = "2px solid red"
-    }
-    else{
-        ageErr.innerHTML = ""
-        age.style.outline = "2px solid green"
-    }
+    // if(age.value == ""){
+    //     ageErr.innerHTML = "<font color='red'>Please enter age <font>"
+    //     age.style.outline = "2px solid red"
+    // }
+    // else if(age.value < 18){
+    //     ageErr.innerHTML = "<font color='red'>You are kid... <font>"
+    //     age.style.outline = "2px solid red"
+    // }
+    // else{
+    //     ageErr.innerHTML = ""
+    //     age.style.outline = "2px solid green"
+    // }
 
 
 
     // alert("form submitted....")
     console.log("form submitted....");
+    // console.log(name.value);
+    // console.log(age.value);
+
+
+    var user = {
+        name: name.value,
+        age: age.value
+    }
+
+    console.log(user);
+
+
+    const data = document.getElementById("data")
+    // data.innerHTML = user
+    data.innerHTML = JSON.stringify(user)
+
+    var x = JSON.stringify(user)
+    console.log(x[0]);
 }
