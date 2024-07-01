64a96f5de5627ee145a276c8c61563f069563641
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-01 16:29:55 +0530

    slideshow

---
+++ SlideShow/SlideShow.js
@@ -0,0 +1,27 @@
+var images = ["img1.jpg", "img2.avif", "img3.jpg", "img4.avif", "img5.avif"];
+
+var currentIndex = 0;
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const image = document.getElementById("image");
+
+prev.addEventListener("click",()=>{
+
+
+    // 0 = 0-1 + 5 % 5
+    // 3 = (3 -1 + 5) % 5 = 
+    currentIndex = (currentIndex - 1 + images.length)% images.length;
+
+    image.src = "../Images/"+images[currentIndex]
+
+})
+
+
+next.addEventListener("click",()=>{
+
+    // 0 = 0 + 1 % 5
+    currentIndex = (currentIndex + 1) % images.length
+
+    image.src = "../Images/"+images[currentIndex]
+})
