a9c5805c6a86cce8be67e6970390ae9aaaae1746
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-10 16:42:00 +0530

    date

---
+++ Date/Date.js
@@ -0,0 +1,50 @@
+// Date class in js 
+
+// var date = new Date();
+// var date = new Date(2020, 0,14 , 5,3,40,)
+// var date2 = new Date("2023-01-14T21:30:05Z");
+// var date2 = new Date(2020,1,14)
+// var date2 = new Date(0)
+// console.log(date2);
+
+
+// var date = new Date();
+// var date = new Date(2020,1,14,6,3,4,977)
+// var date3 = new Date("2023-01-14T12:30:12");
+// var date3 = new Date(2023,2,14)
+// var date3 = new Date(0);
+
+// var date = new Date();
+
+// // console.log("date:---> " + date);
+// console.log("month ----> "+date.getDate());
+
+// console.log("year ----> "+date.getFullYear());
+
+// console.log("hours ----> "+date.getHours());
+
+// console.log("minutes ----> "+date.getMinutes());
+
+// console.log("seconds ----> "+date.getSeconds());
+
+// console.log("milliseconds ----> "+date.getMilliseconds());
+
+// console.log("day of the week ----> "+date.getDay());
+
+// console.log("day of the year ----> "+date.getDate());
+
+// console.log("milliseconds since January 1, 1970 ----> "+date.getTime());
+
+
+var date2 = new Date();
+
+date2.setDate(14);
+date2.setMonth(1);
+date2.setFullYear(2022)
+date2.setTime(672388)
+date2.setHours(14)
+date2.setMinutes(36)
+date2.setSeconds(24)
+
+
+console.log(date2);
